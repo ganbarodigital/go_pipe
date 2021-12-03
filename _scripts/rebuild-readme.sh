a64d44b43b701b72a75e10b5137ca6e8bd83775a
Stuart Herbert <stuart@stuartherbert.com>
2021-12-03 16:02:31 +0000

    Tools: add a script to rebuild the README from the godocs

---
+++ _scripts/rebuild-readme.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# run this from the folder where you want the README to appear
+goreadme -constants -factories -functions -methods -types -variabless -title 'Welcome to Pipe!' > README.md
